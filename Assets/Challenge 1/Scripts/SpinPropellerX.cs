8cec67969472771c73fd89d5ef3e0c49f64a4b65
Ashwill <stashphill03@gmail.com>
2022-08-23 16:05:46 +0200

    Initial commit

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(0f, 0f, 200f * Time.deltaTime);
+
+    }
+}
